2bab889d91734db12f0af6628773762c6a2cd616
zelli17 <108748722+zelli17@users.noreply.github.com>
2024-01-17 00:38:17 +0530

    svelte session 1
    Svelte is indeed a confusing subject. I am quite familiar with HTML, CSS, and JavaScript, but I'm still pretty much a newbie. I've kind of understood what makes Svelte unique. It's like creating different Lego blocks of the same shape and kind that can be easily incorporated into any kind of building. The uses of reactive statements and declarative statements are quite interesting, and I'm excited to see what comes next.

---
+++ App.svelte
@@ -0,0 +1,50 @@
+<script>
+	let firstname = "jimmy";
+	let lastname = 'hendrix';
+	let beltcolor = 'black';
+
+	$: fullname = `${firstname} ${lastname}`;
+	$: console.log(beltcolor)
+	
+	const handleclick = () => {
+		beltcolor = 'orange';
+	};
+
+	const handleip = (e) =>{
+		beltcolor = e.target.value;
+	};
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p >{fullname} - {beltcolor}belt</p>
+	<button on:click={handleclick}>update button color</button>
+	<!-- <input type="text" on:input={handleip} value={beltcolor}/> -->
+
+	<input type="text" bind:value={firstname}/>
+	<input type="text" bind:value={lastname}/>
+	<input type="text" bind:value={beltcolor}/>
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
