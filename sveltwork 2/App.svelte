040f6b3c9ca31042f30d5abc62ca3d821403f5fd
Aayush Nair <108748722+zelli17@users.noreply.github.com>
2024-01-18 19:42:45 +0530

    Add files via upload
    This is my second session with Svelte, and there are many things that make Svelte quite enigmatic. The seamless interaction between HTML and JavaScript is quite interesting. Today, I learned about running loops in Svelte. It's quite odd to see HTML perform loop functions, considering it is a markup language. I also learned how to create an inline function to delete data and display it on the webpage. Next, I will be learning conditional statements in Svelte

---
+++ sveltwork 2/App.svelte
@@ -0,0 +1,45 @@
+<script>
+	let people =[
+	{name:'aayush', beltColor:'black', age: 43, id:1},
+	{name:'sayush', beltColor:'orange', age: 99, id:2},
+	{name:'atyush', beltColor:'green', age: 23, id:3}
+	];
+
+	const handleClick = (id) => {
+		people = people.filter((person) => person.id != id)
+	}
+</script>
+
+<main>
+	{#each people as person (person.id) }
+	<div>
+		<h2>{person.name}</h2>
+		<h3>{person.beltColor} belt and age is {person.age}</h3>
+		<button on:click={() => handleClick(person.id)}>Delete this</button>
+	</div>
+	{:else}
+		<p>no more people remaining</p>
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
